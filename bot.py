617eaa047d53190057296cbcfea04fc0a7faf2d8
iiLarsH <Larshommersom@gmail.com>
2023-04-25 20:35:44 +0200

    Basic functionality
    !roll
    !ping
    
    Hiding token

---
+++ bot.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+import discord
+import os
+import asyncio
+from discordtoken import discordtoken
+
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("Succes: Bot has started working")
+
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with client:
+        await load()
+        await client.start(discordtoken.get())
+
+asyncio.run(main())
---
+++ cogs/roll.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import random
+
+class Roll(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("roll.py is ready")
+
+    @commands.command()
+    async def roll(self, ctx):
+        rolled = random.randint(1,100)
+        rating = ""
+
+        if(rolled == 100):
+            rating = "What a chad"
+        elif(rolled < 100 and rolled > 90):
+            rating = "Almost"
+        elif(rolled <= 90 and rolled > 80):
+            rating = "Good"
+        elif(rolled <= 80 and rolled > 69):
+            rating = "Alright"
+        elif(rolled == 69):
+            rating = "Nice"
+        elif(rolled < 69 and rolled > 50):
+            rating = "Decent"
+        elif(rolled <= 50 and rolled > 25):
+            rating = "Meh"
+        elif(rolled <= 25 and rolled > 1):
+            rating = "Almost"
+        elif(rolled == 1):
+            rating = "LOL"
+
+        message = "You rolled " + str(rolled) + ", " + rating
+
+        await ctx.send(message)
+
+
+async def setup(client):
+    await client.add_cog(Roll(client))
