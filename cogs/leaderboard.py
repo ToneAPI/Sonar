6f96e5b9437a21a06e1ebb3eb52bea7e33425ff2
iiLarsH <Larshommersom@gmail.com>
2023-05-04 23:14:21 +0200

    Leaderboard now has Server filter
    Leaderboard got the new messagehandler to speed it up and make it less likely to break
    
    KD and AVG_distance arent currently possible

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -11,106 +11,185 @@ class Leaderboard(commands.Cog):
         print("Leaderboard.py is ready")
 
     @commands.command(aliases = ["lb", "board", "ranking", "rank"])
-    async def leaderboard(self, ctx, message, weapon=""):
-        message = message.lower()
-        weaponid = ""
-        botmessage = "Leaderboard of " + message + "\n"
-        if(weapon != ""):
-            weaponid = self.getweaponid(weapon)
-            message = message[:-1]
-            if(message != "deaths"):
-                inbetween = " with "    
-            else: 
-                inbetween = " to "    
-            botmessage = "Leaderboard of " + message + inbetween + weapon + "\n"
-        
-        if(str(message) not in ["kills", "deaths", "max_distance", "total_distance"]):
-            await ctx.send('```This filter doesnt exist```')
-        print(message)
-        
+    async def leaderboard(self, ctx, *message):
+        boards = ["maxd", "totald","deaths", "kills"]
+        error = False
+        board = ""
+        servers = []
+        server = ""
+        weaponid= ""
+        weaponname= ""
 
-        payload = {"weapon": weaponid}
+        if(message == ()):
+            error = True
+            botmessage = "No leaderboard option selected, choose from:\n"
+            for i in boards:
+                botmessage = botmessage + i + "\n"
+            await ctx.send(botmessage)
+
+        for i in message:
+            item = i.replace(",", "")
+            if(item.lower() not in boards):
+                function_dict = self.message_handler(item)
+                if(function_dict['weapon'] != None):
+                    weaponname = function_dict["weapon"][1]
+                    weaponid= function_dict['weapon'][0]
+                if(function_dict['server'] != None):
+                    servers.append(function_dict['server'])
+            else:
+                board = item 
+
+        if(board == "" and error == False):
+            error = True
+            botmessage = "No leaderboard option selected, choose from:\n"
+            for i in boards:
+                botmessage = botmessage + i + "\n"
+            await ctx.send(botmessage)
+
+        if(len(servers) != 0):           
+            if(len(servers) > 1):   
+                for i in servers:
+                    if(i in " ".join(message)):
+                        server = i
+                        break   
+            else:
+                server = servers[0]
+
+        botmessage = self.getleaderboard(weaponid=weaponid, weaponname=weaponname, server=server, board=board)
+
+        await ctx.send(f'```{botmessage}```')
+
+    def message_handler(self, message):
+        weapon = False
+        server = False
+        return_message = {"weapon": None, "server": None}
+
+        weaponid, weaponname = self.getweaponid(message)
+        if(weaponid != ""):
+            weapon = True
+
+        servername = self.getserver(message)
+        if(servername != ""):
+            server = True
+
+        if(server):
+            return_message["server"] = servername
+        if(weapon):
+            return_message["weapon"] = [weaponid, weaponname]
+
+        return return_message
+
+    def getweaponid(self, weaponname):
+        weapons ={
+                    "execution": "human_execution", 
+                    "car": "mp_weapon_car",
+                    "charge rifle": "mp_weapon_defender",
+                    "double take": "mp_weapon_doubletake",
+                    "epg": "mp_weapon_epg",
+                    "volt": "mp_weapon_hemlok_smg",
+                    "lstar": "mp_weapon_lstar",
+                    "r97": "mp_weapon_r97",
+                    "r-201": "mp_weapon_rspn101",
+                    "smart pistol": "mp_weapon_smart_pistol",
+                    "kraber": "mp_weapon_sniper",
+                    "flatline": "mp_weapon_vinson",
+                    "re-45 auto": "mp_weapon_autopistol",
+                    "alternator": "mp_weapon_alternator_smg",
+                    "frag grenade": "mp_weapon_frag_grenade",
+                    "g2": "mp_weapon_g2",
+                    "spitfire": "mp_weapon_lmg",
+                    "smr": "mp_weapon_smr",
+                    "softball": "mp_weapon_softball",
+                    "firestar": "mp_weapon_thermite_grenade",
+                    "melee": "melee_pilot_emptyhanded",
+                    "masstiff": "mp_weapon_mastiff",
+                    "mgl": "mp_weapon_mgl",
+                    "r-101": "mp_weapon_rspn101_og",
+                    "wingman": "mp_weapon_wingman",
+                    "invalid": "invalid",
+                    "thunderbolt": "mp_weapon_arc_launcher",
+                    "dmr": "mp_weapon_dmr",
+                    "electric smoke": "mp_weapon_grenade_electric_smoke",
+                    "cold war": "mp_weapon_pulse_lmg",
+                    "satchel": "mp_weapon_satchel",
+                    "eva-8 auto": "mp_weapon_shotgun",
+                    "mozambique": "mp_weapon_shotgun_pistol",
+                    "wingman elite": "mp_weapon_wingman_n",
+                    "gravity star": "mp_weapon_grenade_gravity",
+                    "pulse blade": "mp_weapon_grenade_sonar",
+                    "archer": "mp_weapon_rocket_launcher",
+                    "outOfBounds": "outOfBounds",
+                    "mind crime": "mind_crime",
+                    "devotion": "mp_weapon_esaw",
+                    "fall": "fall",
+                    "p2016": "mp_weapon_semipistol",
+                    "arc grenade": "mp_weapon_grenade_emp",
+                    "hemlok": "mp_weapon_hemlok",
+                    "phase shift": "phase_shift"
+                }
+    
+        weaponid = ""
+        for weapon in weapons:     
+            if(weaponname.lower() in weapon.lower()):     
+                weaponid = weapons[weapon]
+                weaponname = weapon
+                break
+
+        return weaponid, weaponname
+
+    def getserver(self, snippet):
+        server = ""
+        response = requests.get('https://tone.sleepycat.date/v2/client/servers').json()
+        servers = response.keys()
+
+        for i in servers:
+            if(snippet.lower() in i.lower()):
+                server = i
+                break
+
+        return server
+    
+    def getleaderboard(self, board, weaponid="", weaponname="", server=""):
+        payload = {"weapon": weaponid, "server": server}
         response = requests.get('https://tone.sleepycat.date/v2/client/players', params=payload).json()
         players = response.keys()
 
+        if(board.lower() in ["kills", "deaths", "maxd", "totald"]):
+            if(board.lower() == "totald"):
+                board = "total_distance"
+            if(board.lower() == "maxd"):
+                board = "max_distance"       
+
+
         top10 = []
         while(len(top10)<10):
-            current = 0
+            current = 0 
             name = ""
             for p in players:
                 stats = response[p]
-                checked = stats[str(message)]
-                if(checked > current and str(stats["username"] +": " + str(stats[str(message)])) not in top10):
+                checked = stats[str(board)]
+                if(checked >= current and str(stats["username"] +": " + str(checked)) not in top10):
                      current = checked
                      name = stats['username']
             top10.append(name+ ": " + str(current))
+        if(board.lower() == "deaths"):
+            inbetween = "to"
+        else:
+            inbetween = "with"
+
+        if(server == ""):
+            server = "all servers"
+        if(weaponname == ""):
+            weaponname = "any weapon"
+
+        botmessage = str(f"Leaderboard of {board} {inbetween} {weaponname} for {server}\n-------------------------------\n")
 
         counter = 1
         for i in top10:
             botmessage = botmessage + str(str(counter) + " - " + i + "\n")
             counter= counter+1 
 
-        await ctx.send(f'```{botmessage}```')
-
-    def getweaponid(self, weaponname):
-        response = requests.get('https://tone.sleepycat.date/v2/client/weapons').json()
-        r = response.keys()
-        
-        weapon_dict = {'human_execution': 'execution',
-                    'mp_weapon_car': 'car',
-                    'mp_weapon_defender': 'charge_rifle',
-                    'mp_weapon_doubletake': 'double_take',
-                    'mp_weapon_epg': 'epg',
-                    'mp_weapon_hemlok_smg': 'volt',
-                    'mp_weapon_lstar': 'lstar',
-                    'mp_weapon_r97':'r97',
-                    'mp_weapon_rspn101': 'r-201',
-                    'mp_weapon_smart_pistol': 'smart_pistol',
-                    'mp_weapon_sniper': 'kraber',
-                    'mp_weapon_vinson': 'flatline',
-                    'mp_weapon_autopistol': 're-45_auto',
-                    'mp_weapon_alternator_smg': 'alternator',
-                    'mp_weapon_frag_grenade': 'frag_grenade',
-                    'mp_weapon_g2': 'g2',
-                    'mp_weapon_lmg': 'spitfire',
-                    'mp_weapon_smr': 'smr',
-                    'mp_weapon_softball': 'softball',
-                    'mp_weapon_thermite_grenade': 'firestar',
-                    'pilot_emptyhanded': 'melee',
-                    'mp_weapon_mastiff': 'masstiff',
-                    'mp_weapon_mgl': 'mgl',
-                    'mp_weapon_rspn101_og': 'r-101',
-                    'mp_weapon_wingman': 'wingman',
-                    'invalid': 'invalid',
-                    'mp_weapon_arc_launcher': 'thunderbolt',
-                    'mp_weapon_dmr': 'dmr',
-                    'mp_weapon_grenade_electric_smoke': 'electric_smoke',
-                    'mp_weapon_pulse_lmg': 'cold_war',
-                    'mp_weapon_satchel': 'satchel',
-                    'mp_weapon_shotgun': 'eva-8_auto',
-                    'mp_weapon_shotgun_pistol': 'mozambique', 
-                    'mp_weapon_wingman_n': 'wingman_elite',
-                    'mp_weapon_grenade_gravity': 'gravity_star',
-                    'mp_weapon_grenade_sonar': 'pulse_blade',
-                    'mp_weapon_rocket_launcher': 'archer',
-                    'outOfBounds': 'outOfBounds',
-                    'mind_crime': 'mind_crime',
-                    'mp_weapon_esaw': 'devotion',
-                    'fall': 'fall',
-                    'mp_weapon_semipistol': 'p2016',
-                    'mp_weapon_grenade_emp': 'arc_grenade',
-                    'mp_weapon_hemlok': 'hemlok',
-                    'melee_pilot_emptyhanded': 'melee',
-                    'phase_shift': 'phase_shift'
-                    }
-        weaponid = ""
-        for i in r:
-            if(i in weapon_dict.keys()):  
-                if (weaponname.lower() == weapon_dict[i]):
-                    weaponid = i
-                    break
-
-        return weaponid
+        return botmessage
 
 async def setup(client):
     await client.add_cog(Leaderboard(client))
